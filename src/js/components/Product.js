14245b758ea33da62937799bc720f8bc28258a35
Szymon Wierucki <szymon.wierucki@gmail.com>
2021-07-20 20:35:52 +0200

    Add correct js code for task 10.3

--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -197,7 +197,7 @@ class Product{
 
     const event = new CustomEvent('add-to-cart', {
       bubbles: true,
-      details: {
+      detail: {
         product: thisProduct.prepareCartProduct(),
       },
     });
